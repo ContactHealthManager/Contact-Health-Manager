694adfa769be00494ef748306025d66dce4e81b5
jaebin1234 <jaebin2586@gmail.com>
2020-09-24 21:12:46 +0900

    board 데이터베이스 수정 ,추가

--- src/main/java/com/victolee/board/domain/entity/BoardEntity.java
+++ src/main/java/com/victolee/board/domain/entity/BoardEntity.java
@@ -14,8 +14,8 @@ import javax.persistence.*;
 public class BoardEntity extends TimeEntity {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pId;
 
     @Column(length = 10, nullable = false)
     private String writer;
@@ -26,11 +26,39 @@ public class BoardEntity extends TimeEntity {
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
+    @Column(length = 13, nullable = false)
+    private String cNumber;
+
+    @Column(length = 10, nullable = false)
+    private String cName;
+
+    @Column(nullable = false)
+    private Integer postCount;
+
+    @Column(nullable = false)
+    private Integer postLike;
+
+    @Column(nullable = false)
+    private Integer commentId;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String pAddress;
+
+
     @Builder
-    public BoardEntity(Long id, String title, String content, String writer) {
-        this.id = id;
+    public BoardEntity(Long pId, String title, String content,
+                       String writer,String cNumber,String cName,
+                       Integer postCount,Integer postLike,Integer commentId,
+                       String pAddress) {
+        this.pId = pId;
         this.writer = writer;
         this.title = title;
         this.content = content;
+        this.cNumber = cNumber;
+        this.cName = cName;
+        this.postCount = postCount;
+        this.postLike = postLike;
+        this.commentId = commentId;
+        this.pAddress = pAddress;
     }
 }
