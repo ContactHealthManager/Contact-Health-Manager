5157038573540f6d5e958778684748e7353ff30d
jaebin1234 <jaebin2586@gmail.com>
2020-10-27 16:57:58 +0900

    내정보 및 로그인 ,회원가입 개선

--- src/main/java/com/victolee/board/controller/BoardController.java
+++ src/main/java/com/victolee/board/controller/BoardController.java
@@ -25,7 +25,7 @@ public class BoardController {
         return "/index";
     }
 
-    /* 게시글 목록 */
+    /* 게시글 목록 */ /*페이지를 담는 리스트와 그 페이지 안의 목록들을 담은 리스트 */
     @GetMapping("/managerlist")
     public String list(Model model, @RequestParam(value="page", defaultValue = "1") Integer pageNum) {
         List<BoardDto> boardList = boardService.getBoardlist(pageNum);
@@ -39,23 +39,23 @@ public class BoardController {
 
 
 
-    /* 게시글 상세 */
+    /* 게시글 상세 목록*/
     @GetMapping("/post/{no}")
     public String detail(@PathVariable("no") Long no, Model model) {
-        BoardDto boardDTO = boardService.getPost(no);
+        BoardDto boardDto = boardService.getPost(no);
 
-        model.addAttribute("boardDto", boardDTO);
+        model.addAttribute("boardDto", boardDto);
         return "board/detail";
     }
 
 
-    /* 게시글 쓰기 */
+    /* 게시글 쓰기 폼으로 이동*/
     @GetMapping("/post")
     public String write() {
         return "board/write";
     }
 
-    /* 게시글 쓰기 */
+    /* 게시글 쓰기 */ /* 로그인한 유저가 작성자가 되도록 해줌.*/
     @RequestMapping(value = "/post", method = RequestMethod.POST)
 
     public String write(BoardDto boardDto, Principal principal) {
@@ -69,22 +69,21 @@ public class BoardController {
     }
 
 
-
     /* 게시글 수정 */
     @GetMapping("/post/edit/{no}")
     public String edit(@PathVariable("no") Long no, Model model) {
-        BoardDto boardDTO = boardService.getPost(no);
+        BoardDto boardDto = boardService.getPost(no);
 
-        model.addAttribute("boardDto", boardDTO);
+        model.addAttribute("boardDto", boardDto);
         return "board/update";
     }
     /* 수정 폼에서 수정 완료*/
     @PutMapping("/post/edit/{no}")
-    public String update(BoardDto boardDTO,Principal principal) {
+    public String update(BoardDto boardDto,Principal principal) {
         String userid = principal.getName();
 
-        boardDTO.setWriter(userid);
-        boardService.savePost(boardDTO);
+        boardDto.setWriter(userid);
+        boardService.savePost(boardDto);
 
         return "redirect:/managerlist";
     }
@@ -96,6 +95,7 @@ public class BoardController {
 
         return "redirect:/managerlist";
     }
+
     /* 게시글 검색 */
     @GetMapping("/board/search")
     public String search(@RequestParam(value="keyword") String keyword, Model model) {
