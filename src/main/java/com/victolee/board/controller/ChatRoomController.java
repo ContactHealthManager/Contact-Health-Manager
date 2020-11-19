033d434fc3970ab7ffe79695e7baabb142b2c886
LeeSeongSu <oplm123456@naver.com>
2020-11-19 19:38:05 +0900

    chatroom,chatmassage

--- src/main/java/com/victolee/board/controller/ChatRoomController.java
+++ src/main/java/com/victolee/board/controller/ChatRoomController.java
@@ -20,11 +20,12 @@ public class ChatRoomController {
     private final ChatRoomRepository chatRoomRepository;
     private final JwtTokenProvider jwtTokenProvider;
 
+    // 채팅 리스트 화면
     @GetMapping("/room")
     public String rooms(Model model) {
         return "/chat/room";
     }
-
+    // 모든 채팅방 목록 반환
     @GetMapping("/rooms")
     @ResponseBody
     public List<ChatRoom> room() {
@@ -33,25 +34,25 @@ public class ChatRoomController {
         return chatRooms;
     }
 
-
+    // 채팅방 생성
     @PostMapping("/room")
     @ResponseBody
     public ChatRoom createRoom(@RequestParam String name) {
         return chatRoomRepository.createChatRoom(name);
     }
-
+    // 채팅방 입장 화면
     @GetMapping("/room/enter/{roomId}")
     public String roomDetail(Model model, @PathVariable String roomId) {
         model.addAttribute("roomId", roomId);
         return "/chat/roomdetail";
     }
-
+    // 특정 채팅방 조회
     @GetMapping("/room/{roomId}")
     @ResponseBody
     public ChatRoom roomInfo(@PathVariable String roomId) {
         return chatRoomRepository.findRoomById(roomId);
     }
-
+    //로그인 유저 정보 조회
     @GetMapping("/user")
     @ResponseBody
     public UserInfoDto getUserInfo(Principal principal) {
