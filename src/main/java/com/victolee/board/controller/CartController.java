c2accc010cdf599a1c52f8d39885e5400c8c58a3
jhj960918 <65895403+jhj960918@users.noreply.github.com>
2020-11-19 19:40:30 +0900

    형준 chat 엔티티 생성

--- src/main/java/com/victolee/board/controller/CartController.java
+++ src/main/java/com/victolee/board/controller/CartController.java
@@ -37,7 +37,7 @@ public class CartController {
 
         String loginId = principal.getName();
         List<CartDto> cartList1 = cartService.getCartlistUser(loginId);
-        Set<CartDto> cartList =  Sets.newHashSet(cartList1);gi
+        Set<CartDto> cartList =  Sets.newHashSet(cartList1);
         System.out.println("씨발"+cartList);
 
         model.addAttribute("cartList", cartList);
