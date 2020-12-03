e2cb746462b917c55369dc67d02c043674c2cb4e
jhj960918 <65895403+jhj960918@users.noreply.github.com>
2020-12-03 22:16:52 +0900

    캡스톤 디자인 완성

---
+++ src/main/java/com/jin/chat/redispubsub/RedisPublisher.java
@@ -0,0 +1,18 @@
+package com.jin.chat.redispubsub;
+
+import com.jin.chat.dto.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RedisPublisher {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void publish(ChannelTopic topic, ChatMessage message) {
+        redisTemplate.convertAndSend(topic.getTopic(), message);
+    }
+}
